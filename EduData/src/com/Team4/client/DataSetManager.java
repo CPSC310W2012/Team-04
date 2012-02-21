45783e0817348a48efdbd8d9039ffb4da1da4a51
Ryan <ryanbooth77@gmail.com>
2012-02-21 15:11:19 -0800

    finished DataSetManager: untested

---
+++ EduData/src/com/Team4/client/DataSetManager.java
@@ -0,0 +1,37 @@
+package com.Team4.client;
+
+import java.util.ArrayList;
+import javax.jdo.annotations.Persistent;
+
+public class DataSetManager {
+	
+	@Persistent
+	private ArrayList<DataSet> dataSets;
+	
+	public void addDataSet( DataSet dSet ) {
+		dataSets.add(dSet);
+	}
+	
+	public DataSet removeDataSet( DataSet dSet ) throws DataSetNotPresentException {
+		for( DataSet iter : dataSets ) {
+			if( iter.getDataSetID() == dSet.getDataSetID() ) {
+				dataSets.remove(iter);
+				return iter;
+			}
+		}
+		throw new DataSetNotPresentException( "Data set not found.");
+	}
+	
+	public ArrayList<DataSet> listAll() {
+		return dataSets;
+	}
+	
+	public DataSet getDataSet( int id ) throws DataSetNotPresentException{
+	    for( DataSet dSet : dataSets ) {
+	    	if ( dSet.getDataSetID() == id )
+	    		return dSet;
+	    }
+	    
+	    throw new DataSetNotPresentException("Data Set not found.");
+	}
+}
