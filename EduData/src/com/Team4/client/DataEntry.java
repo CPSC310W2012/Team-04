878530f0d9597bea159d063dfcf27f83b7d0cd3b
Kaya Gajos <kaya@host66-205.wifi.ubc.ca>
2012-02-21 13:22:40 -0800

    DataEntry Class created

---
+++ EduData/src/com/Team4/client/DataEntry.java
@@ -0,0 +1,45 @@
+package com.Team4.client;
+
+public class DataEntry {
+
+	private String schoolName;
+	private int grade;
+	private int entryID;
+	private String course;
+	
+	public DataEntry(String schName, int stGrade, String courseName){
+		schoolName = schName;
+		grade = stGrade;
+		course = courseName;
+	}
+	
+	
+	public String getSchool(){
+		return schoolName;
+	}
+	
+	public void setSchoolName(String name){
+		schoolName = name;	
+	}
+	
+	public int getGrade(){
+		return grade;
+	}
+	
+	public void setGrade(int stGrade){
+		grade = stGrade;
+	}
+	
+	public int getID(){
+		return entryID;
+	}
+	
+	public String getCourse(){	
+		return course;
+	}
+	
+	public void setCourse(String courseName){
+		course = courseName;	
+	}
+	
+}
