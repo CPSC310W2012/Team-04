1ea2ba249b3685b9b3dc75b805a8767eb63f636d
Tristan Sebens <ubcsignup@hotmail.com>
2012-02-27 15:45:54 -0800

    Fixed one bug. Implemented some functions for displaying the imported data sets

--- EduData/src/com/Team4/client/EduData.java
+++ EduData/src/com/Team4/client/EduData.java
@@ -1,10 +1,14 @@
 package com.Team4.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTMLTable;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Grid;
@@ -23,6 +27,10 @@ public class EduData implements EntryPoint {
 	private Label loginLabel = new Label(
 			"Please sign in to your Google Account to access the EduData application.");
 	private Anchor signInLink = new Anchor("Sign In");
+	
+	// Here we define the system classes
+	DataSetManager dataSetMgr = new DataSetManager();
+	
 
 	public void onModuleLoad() {
 		// Check login status using login service.
@@ -93,18 +101,61 @@ public class EduData implements EntryPoint {
 
 		Button vdButton = new Button("Visualize Data");
 		grid.setWidget(7, 0, vdButton);
-		grid.getCellFormatter().setHorizontalAlignment(5, 0,
-				HasHorizontalAlignment.ALIGN_CENTER);
-		grid.getCellFormatter().setHorizontalAlignment(7, 0,
-				HasHorizontalAlignment.ALIGN_CENTER);
-		grid.getCellFormatter().setHorizontalAlignment(9, 0,
-				HasHorizontalAlignment.ALIGN_CENTER);
+
 
 		Label yaDescrip = new Label("View and edit account information");
 		grid.setWidget(8, 0, yaDescrip);
 
 		Button yaButton = new Button("Your Account");
 		grid.setWidget(9, 0, yaButton);
+		
+		// Here we set the alignment of the page
+		setAlignment( grid );
+		
+	}
+	
+	/**
+	 * Helper method to the EduData class. 
+	 * Takes in a FlexTable as a parameter and adds all DataSets currently 
+	 * held by the DataSetManager
+	 * @param table The FlexTable to be updated 
+	 * @return The newly updated FlexTable
+	 * */
+	private FlexTable updateDataSetDisplay( FlexTable table ) {
+		formatFlexTable( table );
+		ArrayList<DataSet> dataSets = this.dataSetMgr.listAll();
+		
+		int a = 1;
+		for ( DataSet dSet : dataSets ) {
+			table.setText( a , 0 , dSet.getName() );
+			a++;
+		}
+		
+		return table;
+	}
+	
+	// Just a method to keep the updateDataSetDisplay method clean
+	private void formatFlexTable( FlexTable table ) {		
+		// Here, we set the lables for the columns of data
+		table.setTitle( "Imported Data Sets" );
+		table.setText( 0, 0, "Data Set Name" );
+		
+		//TODO: The table's appearance should be much nicer
+		// Here, we tune the finer points of the data's display 
+		table.setBorderWidth( 1 );
+		table.setCellPadding( 4 );
+	}
+	
+	/**
+	 * Helper method to the EduData class. 
+	 * */
+	private void setAlignment(Grid grid) {		
+		grid.getCellFormatter().setHorizontalAlignment(5, 0,
+				HasHorizontalAlignment.ALIGN_CENTER);
+		grid.getCellFormatter().setHorizontalAlignment(7, 0,
+				HasHorizontalAlignment.ALIGN_CENTER);
+		grid.getCellFormatter().setHorizontalAlignment(9, 0,
+				HasHorizontalAlignment.ALIGN_CENTER);
 		grid.getCellFormatter().setHorizontalAlignment(2, 0,
 				HasHorizontalAlignment.ALIGN_CENTER);
 		grid.getCellFormatter().setHorizontalAlignment(4, 0,
@@ -133,7 +184,7 @@ public class EduData implements EntryPoint {
 				HasVerticalAlignment.ALIGN_TOP);
 		grid.getCellFormatter().setVerticalAlignment(8, 0,
 				HasVerticalAlignment.ALIGN_BOTTOM);
-
 	}
+	
 
 }
