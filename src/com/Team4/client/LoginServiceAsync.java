adac22d6a5a1726415539a9c37e9bee4e4257bca
Ryan <ryanbooth77@gmail.com>
2012-03-20 17:41:54 -0700

    picking up after achowchow

--- src/com/Team4/client/LoginServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.Team4.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync {
-  public void login(String requestUri, AsyncCallback<LoginInfo> async);
-}
